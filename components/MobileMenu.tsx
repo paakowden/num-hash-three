098ee6d0ee139a92b69d7c466a11709487b393c0
paakowden <paakow1114@gmail.com>
2024-09-25 20:15:40 +0000

    nav

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -10,12 +10,12 @@ const MobileMenu = () => {
         <AlignJustify className="cursor-pointer" />
       </SheetTrigger>
       <SheetContent>
-        <div className="flex flex-col items-center justify-between h-full py-8">
+        <div className="flex flex-col items-center justify-between h-full py-6">
           <div className="flex flex-col items-center gap-y-32">
             <Logo />
             <div
               className="text-xl font-semibold uppercase 
-          py-10 m-6 rounded-3xl cursor-pointer "
+          py-8 m-6 rounded-3xl cursor-pointer mt-[-80px]"
             >
               <ul className="flex flex-col items-center gap-10">
                 <li>

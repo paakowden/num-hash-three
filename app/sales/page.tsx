6a9add1255c568839123850c8b698871115c5dc9
paakowden <paakow1114@gmail.com>
2024-10-17 14:00:57 +0000

    momo added

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { desVariants, tagVariants } from "@/utils/animate";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const page = () => {
   return (
     <div className="px-6 py-24 sm:py-32 lg:px-8">
+      <motion.div
+        initial="offscreen"
+        whileInView={"onscreen"}
+        variants={desVariants}
+        className="flex justify-center items-center"
+        //mx-auto max-w-2xl text-center
+      >
+        <Image
+          src="/momo.jpg"
+          width={250}
+          height={200}
+          alt="momo"
+          className="rounded-r-full"
+        />
+      </motion.div>
+      <br />
       <div className="mx-auto max-w-2xl text-center">
         <motion.h2
           initial="offscreen"
@@ -15,6 +32,15 @@ const page = () => {
           Contact Sales
         </motion.h2>
       </div>
+      <br />
+      <motion.h3
+        initial="offscreen"
+        whileInView={"onscreen"}
+        variants={tagVariants}
+        className="items-center text-center text-xl"
+      >
+        We accept MTN MoMo Here. MoMo numbers are displayed below:
+      </motion.h3>
       <br />
       <motion.h3
         initial="offscreen"

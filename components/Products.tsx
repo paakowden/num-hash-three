5d5979097e5b410eed29ed8fe4f4c419786a0df2
paakowden <paakow1114@gmail.com>
2024-10-26 17:48:06 +0000

    changed price

--- components/Products.tsx
+++ components/Products.tsx
@@ -10,25 +10,25 @@ const products = [
     id: "1",
     img: "/images/prodhome.jpg",
     title: "Luxury taste in a bottle",
-    price: "$15.00",
+    price: "$24.00",
   },
   {
     id: "2",
     img: "/images/prodhome1.jpg",
     title: "Luxury taste in a bottle",
-    price: "$15.00",
+    price: "$24.00",
   },
   {
     id: "3",
     img: "/images/prodhome2.jpg",
     title: "Luxury taste in a bottle",
-    price: "$15.00",
+    price: "$24.00",
   },
   {
     id: "4",
     img: "/images/prodhome3.jpg",
     title: "Luxury taste in a bottle",
-    price: "$15.00",
+    price: "$24.00",
   },
 ];
 
